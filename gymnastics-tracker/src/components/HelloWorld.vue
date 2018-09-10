13b37389b3abf8667108d8f94a577ac0052b0c54
jbays <jjbaize@gmail.com>
2018-09-10 15:39:22 -0500

    Adding the first 10 buttons for gymnastics tracker

--- gymnastics-tracker/src/components/HelloWorld.vue
+++ gymnastics-tracker/src/components/HelloWorld.vue
@@ -1,8 +1,20 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <h2></h2>
+    <h2>Progressions</h2>
+    <a href="#" class="myButton">front lever</a>
+    <a href="#" class="myButton">straddle planche</a>
+    <a href="#" class="myButton">side lever</a>
+    <a href="#" class="myButton">manna</a>
+    <a href="#" class="myButton">single leg squat</a>
+    <a href="#" class="myButton">hollow back press</a>
+    <a href="#" class="myButton">rope climb</a>
+    <br>
+    <br>
+    <h2>Stretches</h2>
+    <a href="#" class="myButton">front split</a>
+    <a href="#" class="myButton">middle split</a>
+    <a href="#" class="myButton">thoracic bridge</a>
   </div>
 </template>
 
@@ -11,7 +23,7 @@ export default {
   name: 'HelloWorld',
   data() {
     return {
-      msg: 'Welcome to Your Vue.js App',
+      msg: 'Track Your Gymnastics',
     };
   },
 };
@@ -32,5 +44,27 @@ li {
 }
 a {
   color: #42b983;
+}
+.myButton {
+  background-color:#44c767;
+  -moz-border-radius:28px;
+  -webkit-border-radius:28px;
+  border-radius:28px;
+  border:1px solid #18ab29;
+  display:inline-block;
+  cursor:pointer;
+  color:#ffffff;
+  font-family:Arial;
+  font-size:25px;
+  padding:16px 31px;
+  text-decoration:none;
+  text-shadow:0px 1px 0px #2f6627;
+}
+.myButton:hover {
+  background-color:#5cbf2a;
+}
+.myButton:active {
+  position:relative;
+  top:1px;
 }
 </style>
