8416fdb6d3b108e23405cc7cd551c108d05bb23e
jbays <jjbaize@gmail.com>
2018-09-12 08:27:40 -0500

    Mocked out the front end, conceptually.  The presentation pieces are basically in place.  I just need to wash, rinse, and repeat for the rest of the progressions and stretches.

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,13 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import HelloWorld from '@/components/HelloWorld';
+import Homepage from '@/components/Homepage';
+
+import Calendar from '@/components/Calendar';
+import Progressions from '@/components/Progressions';
+import Stretches from '@/components/Stretches';
+
+import FrontLever from '@/components/FrontLever';
+import FrontSplit from '@/components/FrontSplit';
 
 Vue.use(Router);
 
@@ -8,8 +15,33 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld,
+      name: 'Homepage',
+      component: Homepage,
+    },
+    {
+      path: '/calendar',
+      name: 'Calendar',
+      component: Calendar,
+    },
+    {
+      path: '/progressions',
+      name: 'Progressions',
+      component: Progressions,
+    },
+    {
+      path: '/stretches',
+      name: 'Stretches',
+      component: Stretches,
+    },
+    {
+      path: '/progressions/front-lever',
+      name: 'FrontLever',
+      component: FrontLever,
+    },
+    {
+      path: '/stretches/front-split',
+      name: 'FrontSplit',
+      component: FrontSplit,
     },
   ],
 });
