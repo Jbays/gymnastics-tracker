51368a84db737a86cc3ef45d108ec25eb5c35952
jbays <jjbaize@gmail.com>
2018-10-01 17:11:11 -0500

    Stretch seed data successfully inserts into the database.  Everything appears to be in correct shape!  Now I just need to attach db to the front end.

--- scripts/makeStretchSeeds.js
+++ scripts/makeStretchSeeds.js
@@ -1,8 +1,12 @@
 const rawStretchData = require('../data/raw_stretches_data');
 const config = require('../knexfile')['development'];
 const knex = require('knex')(config);
+const _ = require('underscore');
+const allExercisesInPlainLanguage = _.invert(require('../data/exercises_PL'));
+
 const fs = require('fs');
 
+const threeStretches = ['front split','middle split','thoracic bridge'];
 const stretchesOnBothSides = [
   'kneeling achilles',
   'standing achilles',
@@ -15,40 +19,47 @@ const stretchesOnBothSides = [
   'capsule stretch'
 ];
 
-function makeSimpleObject(array){
-  let output = {};
-  output['front split'] = {}
-  output['middle split'] = {}
-  output['thoracic bridge'] = {}
-
-  console.log(array)
-
-  //I just realized that stretch progression don't really fit
-  //in the progessions_exercises_mastery table
-  //I think I need a stretches_exercises_mastery table
-  //with just three progression_ids.
-
-  //30 SEPT 2018 --> LET ME ADD A TABLE TO THE ERD!
-  array.forEach((singleObject)=>{
-    console.log("singleObject['front split']",singleObject['front split']);
-    console.log("singleObject['front split']",!!singleObject['front split']);
-    if ( !!singleObject['front split'] ) {
-      output['front split'][singleObject.FIELD1] = {
-
+/*
+  outputs object of form:
+    stretch_name:{
+      stretch_sequence:{
+        exercise_name: NAME,
+        proficiency_standard: STANDARD
       }
     }
-    // if ( singleObject['middle split'] ) {
+*/
 
-    // }
-    // if ( singleObject['thoracic bridge'] ) {
-      
-    // }
+function collectAllSeedData(array,obj){
+  let output = {};
+
+  threeStretches.forEach((singleStretch)=>{
+    output[singleStretch] = {};
   })
 
+  array.forEach((singleObject)=>{
+    threeStretches.forEach((singleStretch)=>{
+      if ( !!singleObject[singleStretch].exercise_name ) {
+        let exerciseName = singleObject[singleStretch].exercise_name;
+        output[singleStretch][singleObject.FIELD1]= {
+          exercise_name:exerciseName,
+          proficiency_standard:singleObject[singleStretch].duration
+        };
+        if ( stretchesOnBothSides.indexOf(exerciseName) > -1 ){
+          output[singleStretch][singleObject.FIELD1].proficiency_standard += '@';
+        }
+        //final remap
+        output[singleStretch][singleObject.FIELD1].exercise_name = allExercisesInPlainLanguage[exerciseName];
+        output[singleStretch][singleObject.FIELD1].proficiency_standard = obj[output[singleStretch][singleObject.FIELD1].proficiency_standard]
+      };
+    });
+  });
   return output;
 }
 
-// console.log(rawStretchData);
+/*
+  NOTE: mastery_PL.js exists so that I do NOT have to go knex('mastery') etc!
+        Instead, I can directly access the data from an object
+*/
 
 //fetch all mastery ids above 18
 //for each element in rawStretchData
@@ -58,8 +69,42 @@ knex('mastery')
   .select()
   .where('mastery_id','>',18)
   .then((allMasteryIds)=>{
-    let object = makeSimpleObject(rawStretchData);
-    console.log(rawStretchData);
-    console.log(allMasteryIds);
+    let masteryToStandardMap = allMasteryIds.reduce((first,second)=>{
+      let key = second.proficiency_standard.split('x')[1];
+      first[key] = second.mastery_id;
+      return first;
+    },{})
+
+    //all data is now collected.  Now turn each key-value pair into an object
+    //to be inserted into the stretches_progressions_mastery table
+    let object = collectAllSeedData(rawStretchData,masteryToStandardMap);
+    let outputToFile = [];
+    let progression_id = 8;
+    //front split     progression_id = 8
+    //middle split    progression_id = 9
+    //thoracic bridge progression_id = 10
+    for ( stretch in object ) {
+      for ( sequenceNumber in object[stretch] ) {
+        let obj = {
+          progression_id,
+          exercise_id:null,
+          stretch_sequence:null,
+          mastery_id:null
+        };
+        obj.exercise_id = Number(object[stretch][sequenceNumber].exercise_name);
+        obj.stretch_sequence = Number(sequenceNumber);
+        obj.mastery_id = object[stretch][sequenceNumber].proficiency_standard;
+        outputToFile.push(obj);
+      };
+      progression_id++;
+    };
+
+    const writeData = `const ALL_STRETCHES_PROGRESSIONS = ${JSON.stringify(outputToFile)} \n module.exports = ALL_STRETCHES_PROGRESSIONS`;
+
+    fs.writeFile('data/processedStretchSeeds.js',writeData,(err)=>{
+      if(err){console.error(err)};
+      console.log('seed data written to file!');
+    })
+
     knex.destroy();
   })
--- seeds/stretches_progressions_mastery.js
+++ seeds/stretches_progressions_mastery.js
@@ -1,8 +1,8 @@
+const allStretchSeedData = require('../data/processedStretchSeeds');
+
 exports.seed = (knex, Promise) =>{
   return knex('stretches_progressions_mastery').del()
     .then( () =>{
-      return knex('stretches_progressions_mastery').insert([
-        {progression_id: 1, exercise_id: 350,stretch_sequence:1,mastery_id:19},
-      ]);
+      return knex('stretches_progressions_mastery').insert(allStretchSeedData);
     });
 };
