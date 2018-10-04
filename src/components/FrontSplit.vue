247b18ded426fdb7b79879cedcdd5dd5ea0aa5b2
jbays <jjbaize@gmail.com>
2018-10-04 10:55:02 -0500

    Data is correctly fetched from the database.  And renders correct.  Clicking smileys correctly toggles the booleans for each stretch.  And clicking submit successfully enters data into the database.  This is core functionality MVP

--- src/components/FrontSplit.vue
+++ src/components/FrontSplit.vue
@@ -2,126 +2,134 @@
   <div class="hello">
     <h2>Stretches - {{msg}}</h2>
     <h3>Today's Date: {{todaysDate}}</h3>
-    <div>heres your last stretch routine!</div>
-    <div v-if='yourLastStretchRoutine'>
-      <div>date of your last workout --> {{yourLastStretchDate}}</div>
-      <div >{{yourLastStretchRoutine}}</div>
+    <div v-if='!submittedStretchRoutine'>
+      <div>heres your last stretch routine!</div>
+      <div v-if='yourLastStretchRoutine'>
+        <div>date of your last workout --> {{yourLastStretchDate}}</div>
+        <div >{{yourLastStretchRoutine}}</div>
+        <div >your last note: {{lastRoutineNote}}</div>
+      </div>
+      <br>
+      <div>This is today's workout:</div>
+      <div v-if='fetchTodaysWorkout' >
+        <div class='display-exercise' id='sequence1'>
+          exercise name: {{todaysWorkout[0].exercise_name}}
+          <label class='emoji-feedback true'>😀</label><label class='emoji-feedback false'>😐</label>
+          <br>
+          standard: {{todaysWorkout[0].standard}}
+          <br>
+        </div>
+        <div class='display-exercise' id='sequence2'>
+          exercise name: {{todaysWorkout[1].exercise_name}}
+          <label class='emoji-feedback true'>😀</label><label class='emoji-feedback false'>😐</label>
+          <br>
+          standard: {{todaysWorkout[1].standard}}
+        </div>
+        <div class='display-exercise' id='sequence3'>
+          exercise name: {{todaysWorkout[2].exercise_name}}
+          <label class='emoji-feedback true'>😀</label><label class='emoji-feedback false'>😐</label>
+          <br>
+          standard: {{todaysWorkout[2].standard}}
+        </div>
+        <div class='display-exercise' id='sequence4'>
+          exercise name: {{todaysWorkout[3].exercise_name}}
+          <label class='emoji-feedback true'>😀</label><label class='emoji-feedback false'>😐</label>
+          <br>
+          standard: {{todaysWorkout[3].standard}}
+        </div>
+        <div class='display-exercise' id='sequence5'>
+          exercise name: {{todaysWorkout[4].exercise_name}}
+          <label class='emoji-feedback true'>😀</label><label class='emoji-feedback false'>😐</label>
+          <br>
+          standard: {{todaysWorkout[4].standard}}
+        </div>
+        <div class='display-exercise' id='sequence6'>
+          exercise name: {{todaysWorkout[5].exercise_name}}
+          <label class='emoji-feedback true'>😀</label><label class='emoji-feedback false'>😐</label>
+          <br>
+          standard: {{todaysWorkout[5].standard}}
+        </div>
+        <div class='display-exercise' id='sequence7'>
+          exercise name: {{todaysWorkout[6].exercise_name}}
+          <label class='emoji-feedback true'>😀</label><label class='emoji-feedback false'>😐</label>
+          <br>
+          standard: {{todaysWorkout[6].standard}}
+        </div>
+        <div class='display-exercise' id='sequence8'>
+          exercise name: {{todaysWorkout[7].exercise_name}}
+          <label class='emoji-feedback true'>😀</label><label class='emoji-feedback false'>😐</label>
+          <br>
+          standard: {{todaysWorkout[7].standard}}
+        </div>
+        <div class='display-exercise' id='sequence9'>
+          exercise name: {{todaysWorkout[8].exercise_name}}
+          <label class='emoji-feedback true'>😀</label><label class='emoji-feedback false'>😐</label>
+          <br>
+          standard: {{todaysWorkout[8].standard}}
+        </div>
+        <div class='display-exercise' id='sequence10'>
+          exercise name: {{todaysWorkout[9].exercise_name}}
+          <label class='emoji-feedback true'>😀</label><label class='emoji-feedback false'>😐</label>
+          <br>
+          standard: {{todaysWorkout[9].standard}}
+        </div>
+        <div class='display-exercise' id='sequence11'>
+          exercise name: {{todaysWorkout[10].exercise_name}}
+          <label class='emoji-feedback true'>😀</label><label class='emoji-feedback false'>😐</label>
+          <br>
+          standard: {{todaysWorkout[10].standard}}
+        </div>
+        <div class='display-exercise' id='sequence12'>
+          exercise name: {{todaysWorkout[11].exercise_name}}
+          <label class='emoji-feedback true'>😀</label><label class='emoji-feedback false'>😐</label>
+          <br>
+          standard: {{todaysWorkout[11].standard}}
+        </div>
+        <div class='display-exercise' id='sequence13'>
+          exercise name: {{todaysWorkout[12].exercise_name}}
+          <label class='emoji-feedback true'>😀</label><label class='emoji-feedback false'>😐</label>
+          <br>
+          standard: {{todaysWorkout[12].standard}}
+        </div>
+        <div class='display-exercise' id='sequence14'>
+          exercise name: {{todaysWorkout[13].exercise_name}}
+          <label class='emoji-feedback true'>😀</label><label class='emoji-feedback false'>😐</label>
+          <br>
+          standard: {{todaysWorkout[13].standard}}
+        </div>
+        <div class='display-exercise' id='sequence15'>
+          exercise name: {{todaysWorkout[14].exercise_name}}
+          <label class='emoji-feedback true'>😀</label><label class='emoji-feedback false'>😐</label>
+          <br>
+          standard: {{todaysWorkout[14].standard}}
+        </div>
+        <div class='display-exercise' id='sequence16'>
+          exercise name: {{todaysWorkout[15].exercise_name}}
+          <label class='emoji-feedback true'>😀</label><label class='emoji-feedback false'>😐</label>
+          <br>
+          standard: {{todaysWorkout[15].standard}}
+        </div>
+        <div class='display-exercise' id='sequence17'>
+          exercise name: {{todaysWorkout[16].exercise_name}}
+          <label class='emoji-feedback true'>😀</label><label class='emoji-feedback false'>😐</label>
+          <br>
+          standard: {{todaysWorkout[16].standard}}
+        </div>
+      </div>
+      <label>Notes I have:</label>
+      <input type="text" id='stretchRoutineNotes'>
+      <br>
+      <br>
+      <a class="myButton" id='submitButton'>submit</a>
     </div>
-    <br>
-    <div>This is today's workout:</div>
-    <div v-if='fetchTodaysWorkout' >
-      <!-- uncomment below to display whole stretch routine!
-      <div>{{todaysWorkout}}</div> 
-      -->
-      <div class='display-exercise' id='sequence1'>
-        exercise name: {{todaysWorkout[0].exercise_name}}
-        <label class='emoji-feedback true'>😀</label><label class='emoji-feedback false'>😐</label>
-        <br>
-        standard: {{todaysWorkout[0].standard}}
-        <br>
-      </div>
-      <div class='display-exercise' id='sequence2'>
-        exercise name: {{todaysWorkout[1].exercise_name}}
-        <label class='emoji-feedback true'>😀</label><label class='emoji-feedback false'>😐</label>
-        <br>
-        standard: {{todaysWorkout[1].standard}}
-      </div>
-      <div class='display-exercise' id='sequence3'>
-        exercise name: {{todaysWorkout[2].exercise_name}}
-        <label class='emoji-feedback true'>😀</label><label class='emoji-feedback false'>😐</label>
-        <br>
-        standard: {{todaysWorkout[2].standard}}
-      </div>
-      <div class='display-exercise' id='sequence4'>
-        exercise name: {{todaysWorkout[3].exercise_name}}
-        <label class='emoji-feedback true'>😀</label><label class='emoji-feedback false'>😐</label>
-        <br>
-        standard: {{todaysWorkout[3].standard}}
-      </div>
-      <div class='display-exercise' id='sequence5'>
-        exercise name: {{todaysWorkout[4].exercise_name}}
-        <label class='emoji-feedback true'>😀</label><label class='emoji-feedback false'>😐</label>
-        <br>
-        standard: {{todaysWorkout[4].standard}}
-      </div>
-      <div class='display-exercise' id='sequence6'>
-        exercise name: {{todaysWorkout[5].exercise_name}}
-        <label class='emoji-feedback true'>😀</label><label class='emoji-feedback false'>😐</label>
-        <br>
-        standard: {{todaysWorkout[5].standard}}
-      </div>
-      <div class='display-exercise' id='sequence7'>
-        exercise name: {{todaysWorkout[6].exercise_name}}
-        <label class='emoji-feedback true'>😀</label><label class='emoji-feedback false'>😐</label>
-        <br>
-        standard: {{todaysWorkout[6].standard}}
-      </div>
-      <div class='display-exercise' id='sequence8'>
-        exercise name: {{todaysWorkout[7].exercise_name}}
-        <label class='emoji-feedback true'>😀</label><label class='emoji-feedback false'>😐</label>
-        <br>
-        standard: {{todaysWorkout[7].standard}}
-      </div>
-      <div class='display-exercise' id='sequence9'>
-        exercise name: {{todaysWorkout[8].exercise_name}}
-        <label class='emoji-feedback true'>😀</label><label class='emoji-feedback false'>😐</label>
-        <br>
-        standard: {{todaysWorkout[8].standard}}
-      </div>
-      <div class='display-exercise' id='sequence10'>
-        exercise name: {{todaysWorkout[9].exercise_name}}
-        <label class='emoji-feedback true'>😀</label><label class='emoji-feedback false'>😐</label>
-        <br>
-        standard: {{todaysWorkout[9].standard}}
-      </div>
-      <div class='display-exercise' id='sequence11'>
-        exercise name: {{todaysWorkout[10].exercise_name}}
-        <label class='emoji-feedback true'>😀</label><label class='emoji-feedback false'>😐</label>
-        <br>
-        standard: {{todaysWorkout[10].standard}}
-      </div>
-      <div class='display-exercise' id='sequence12'>
-        exercise name: {{todaysWorkout[11].exercise_name}}
-        <label class='emoji-feedback true'>😀</label><label class='emoji-feedback false'>😐</label>
-        <br>
-        standard: {{todaysWorkout[11].standard}}
-      </div>
-      <div class='display-exercise' id='sequence13'>
-        exercise name: {{todaysWorkout[12].exercise_name}}
-        <label class='emoji-feedback true'>😀</label><label class='emoji-feedback false'>😐</label>
-        <br>
-        standard: {{todaysWorkout[12].standard}}
-      </div>
-      <div class='display-exercise' id='sequence14'>
-        exercise name: {{todaysWorkout[13].exercise_name}}
-        <label class='emoji-feedback true'>😀</label><label class='emoji-feedback false'>😐</label>
-        <br>
-        standard: {{todaysWorkout[13].standard}}
-      </div>
-      <div class='display-exercise' id='sequence15'>
-        exercise name: {{todaysWorkout[14].exercise_name}}
-        <label class='emoji-feedback true'>😀</label><label class='emoji-feedback false'>😐</label>
-        <br>
-        standard: {{todaysWorkout[14].standard}}
-      </div>
-      <div class='display-exercise' id='sequence16'>
-        exercise name: {{todaysWorkout[15].exercise_name}}
-        <label class='emoji-feedback true'>😀</label><label class='emoji-feedback false'>😐</label>
-        <br>
-        standard: {{todaysWorkout[15].standard}}
-      </div>
-      <div class='display-exercise' id='sequence17'>
-        exercise name: {{todaysWorkout[16].exercise_name}}
-        <label class='emoji-feedback true'>😀</label><label class='emoji-feedback false'>😐</label>
-        <br>
-        standard: {{todaysWorkout[16].standard}}
+    <div v-else>
+      You successfully submitted results for your stretch routine!
+      <br>
+      See you tomorrow!
+      <div>
+        Need to revise todays workout?
       </div>
     </div>
-    <label>Notes I have:</label>
-    <input type="text" id='stretchRoutineNotes'>
-    <br>
-    <br>
-    <a class="myButton" id='submitButton'>submit</a>
     <a href='#/stretches' class="myButton">Back</a>
   </div>
 </template>
@@ -134,15 +142,9 @@ export default {
   data() {
     return {
       fetchTodaysWorkout:false,
+      lastRoutineNote:'',
       msg: 'Front Split',
       progressionId:8,
-      todaysDate: (new Date()).toDateString(),
-      todaysRoutineId:null,
-      todaysWorkout:null,
-      updatedToday:false,
-      userId:1,
-      yourLastStretchDate:null,
-      yourLastStretchRoutine:null,
       stretchResults:{
         0:null,
         1:null,
@@ -161,8 +163,15 @@ export default {
         14:null,
         15:null,
         16:null,
-        // stretch_note:''
-      }
+      },
+      submittedStretchRoutine: false,
+      todaysDate: (new Date()).toDateString(),
+      todaysRoutineId:null,
+      todaysWorkout:null,
+      updatedToday:false,
+      userId:1,
+      yourLastStretchDate:null,
+      yourLastStretchRoutine:null,
     };
   },
   methods:{
@@ -174,8 +183,8 @@ export default {
       .then((response)=>{
         this.todaysRoutineId = response.data.nextRoutineId;
         this.yourLastStretchDate = response.data.lastWorkoutTimestamp.slice(0,10);
-        this.yourLastStretchRoutine = response.data.lastStretchRoutine;
-
+        this.yourLastStretchRoutine = JSON.parse(JSON.stringify(response.data.lastStretchRoutine));
+        this.lastRoutineNote = JSON.stringify(response.data.routineNote);
         return response.data.lastStretchRoutine;
       })
       .then((previousStretchRoutine)=>{
@@ -188,24 +197,16 @@ export default {
       })
       .then(()=>{
         let allEmojiArray = Array.from(document.getElementsByClassName('emoji-feedback'));
-        // console.log(allEmojiArray);
 
-        //lets add event listeners.
         allEmojiArray.forEach((singleEmoji,index)=>{
           let bool = singleEmoji.classList[1];
           singleEmoji.addEventListener('click',()=>{
             let shift = (index % 2 === 0) ? 0 : 1;
             let integerize = Math.round(index/2)-shift;
-            console.log('this is the index',index);
-            console.log('this is integerize',integerize);
-            console.log('this.stretchResults[integerize]',this.stretchResults[integerize])
-            //make sure clicking the smiley correctly toggles the stretches object in data
             this.stretchResults[integerize] = bool;
-            console.log('AFTER this.stretchResults[integerize]',this.stretchResults[integerize])
           });
         });
 
-        //then click the submit button which'll send all that info
         let submitButton = document.getElementById('submitButton');
 
         submitButton.addEventListener('click',()=>{
@@ -214,18 +215,10 @@ export default {
           payload.stretchNote = document.getElementById('stretchRoutineNotes').value;
           payload.routineId = this.todaysRoutineId;
           payload.stretchResults = this.stretchResults;
-
-          console.log('this is payload',payload);
+          this.submittedStretchRoutine = true;
 
           return axios.post(`http://localhost:3000/api/v1/stretches/${this.userId}/${this.progressionId}`,payload)
-            .then((response)=>{
-              console.log('returned from posting!');
-            })
         })
-
-        //insert first into the users_routines table
-        //then insert into the routines
-
         console.log('yay at the end!@')
       });
   }
